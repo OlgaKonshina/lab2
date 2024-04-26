29b4ce8a7b7adf56ae229e0a0601e0cc68e8f5ce
Olga <konshinaolga32@gmail.com>
2024-04-26 19:04:46 +0300

    Update data_creation.py

--- data_creation.py
+++ data_creation.py
@@ -8,13 +8,9 @@ od.download(
 df = pd.read_csv('heart-disease-dataset/heart_statlog_cleveland_hungary_final.csv')
 # разбиваем на тренировочную и валидационную
 x_train, x_val, = train_test_split(df, test_size=0.2,random_state=42)
-# Путь для сохранения файлов train
 
-#path_train = Path('/home/olga/MLOps/lab2/train/train_data.csv')
-#path_train.parent.mkdir(parents=True, exist_ok=True)
-# Путь для сохранения данных test
-#path_test = Path('/home/olga/MLOps/lab2/test/test_data.csv')
-#path_test.parent.mkdir(parents=True, exist_ok=True)
+
+
 
 # сохранение данных в csv
 x_train.to_csv('train.csv', index=False)
