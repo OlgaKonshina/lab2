79fd84985900b2e2b4897fc392a1611d44298e06
Olga <konshinaolga32@gmail.com>
2024-04-26 16:51:57 +0300

    Update data_creation.py

--- data_creation.py
+++ data_creation.py
@@ -17,8 +17,8 @@ x_train, x_val, = train_test_split(df, test_size=0.2,random_state=42)
 #path_test.parent.mkdir(parents=True, exist_ok=True)
 
 # сохранение данных в csv
-x_train.to_csv(train.csv, index=False)
-x_val.to_csv(test.csv, index=False)
+x_train.to_csv('train.csv', index=False)
+x_val.to_csv('test.csv', index=False)
 
 print('Tренировочные данные: сохранены')
 print('Тестовые данные: сохранены')
