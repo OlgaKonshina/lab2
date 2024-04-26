b7d33b472a4a41c4b4a31521e7d5073ae8a1b939
Olga <konshinaolga32@gmail.com>
2024-04-26 16:44:18 +0300

    Update data_creation.py

--- data_creation.py
+++ data_creation.py
@@ -10,15 +10,15 @@ df = pd.read_csv('heart_statlog_cleveland_hungary_final.csv')
 x_train, x_val, = train_test_split(df, test_size=0.2,random_state=42)
 # Путь для сохранения файлов train
 
-path_train = Path('/home/olga/MLOps/lab2/train/train_data.csv')
-path_train.parent.mkdir(parents=True, exist_ok=True)
+#path_train = Path('/home/olga/MLOps/lab2/train/train_data.csv')
+#path_train.parent.mkdir(parents=True, exist_ok=True)
 # Путь для сохранения данных test
-path_test = Path('/home/olga/MLOps/lab2/test/test_data.csv')
-path_test.parent.mkdir(parents=True, exist_ok=True)
+#path_test = Path('/home/olga/MLOps/lab2/test/test_data.csv')
+#path_test.parent.mkdir(parents=True, exist_ok=True)
 
 # сохранение данных в csv
-x_train.to_csv(path_train, index=False)
-x_val.to_csv(path_test, index=False)
+x_train.to_csv(train.csv, index=False)
+x_val.to_csv(test.csv, index=False)
 
-print('Tренировочные данные: ', path_train)
-print('Тестовые данные: ', path_test)
+print('Tренировочные данные: сохранены')
+print('Тестовые данные: сохранены')
